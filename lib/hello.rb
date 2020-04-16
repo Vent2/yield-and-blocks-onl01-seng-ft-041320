01100ef5ef0f826132a7e23b3cce1a1bb625f36c
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 17:46:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,6 +4,6 @@ def hello_t(arr)
   yield(arr)
 end
 
-results1 = hello_t(Array) do |name|
-  name.collect { |e| puts "#{e}" }
+results1 = hello_t(Array) do |arr|
+  arr.collect { |e| puts "#{e}" }
 end
