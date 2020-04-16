4def8c7f282379d1fbdfe2c7ca58243f2ba13ae8
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 18:53:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,10 @@
 def hello_t(array)
   i = 0
-  if i == 0
-    return "Hey! No block was given!"
-    i += 1
-  elsif i < array.length
-    yield array[i]
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
   end
+ 
+  array
 end
